85b6928a0055e046817908b90d02f9797990dae6
Joe Santiago <joe.santiago@partner.sprinklr.com>
2021-03-16 15:55:33 -0500

    Adding tests for Explore

--- packages/explore/configs/explore.conf.js
+++ packages/explore/configs/explore.conf.js
@@ -1,5 +1,5 @@
-exports.host = `https://staging.sprinklr.com`;
-exports.package = `website`;
+exports.host = `http://d31sppokz8rp31.cloudfront.net/modern-care-strategic-handbook.html/`;
+exports.package = `explore`;
 exports.prod = `https://www.sprinklr.com`;
 exports.info = `https://info.sprinklr.com`;
 exports.explore = `https://explore.sprinklr.com`;
---
+++ packages/explore/configs/explore.staging.codecept.conf.js
@@ -0,0 +1,35 @@
+const { setHeadlessWhen } = require('@codeceptjs/configure');
+var myArgs = process.argv.slice(2);
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+exports.config = {
+  tests: '../tests/staging/*_test.js',
+  output: './output',
+  helpers: {
+    Playwright: {
+      url: `https://staging-explore.sprinklr.com.s3-website-us-east-1.amazonaws.com`,
+      show: true,
+      browser: 'chromium'
+    }
+  },
+  include: {
+    I: '../../../steps_file.js'
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'tests',
+  plugins: {
+    pauseOnFail: {},
+    retryFailedStep: {
+      enabled: true
+    },
+    tryTo: {
+      enabled: true
+    },
+    screenshotOnFail: {
+      enabled: true
+    }
+  }
+}
